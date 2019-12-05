f5c3df5c9ea0e243047e4fca26f7c4e9f9d60243
wojciechGaudnik <wojciech.gaudnik@gmail.com>
2019-12-05 11:36:41 +0100

    Added to .gitignore intelij

---
+++ Coloseum/Gladiators/Gladiator.cs
@@ -0,0 +1,11 @@
+namespace Coloseum.Gladiators
+{
+    public abstract class Gladiator
+    {
+        protected int _HP {set; get;}
+        protected int _SP {set; get;}
+        protected int _DEX {set; get;}
+        protected int _LVL {set; get;}
+        protected string _Name {set; get;}
+    }
+}
---
+++ Coloseum/Gladiators/Swordsman.cs
@@ -0,0 +1,7 @@
+namespace Coloseum.Gladiators
+{
+    public class Swordsman
+    {
+        
+    }
+}
